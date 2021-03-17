476473de4aaa830a5c61d4eca238defffcbab8de
heydhal <fadhal.shulhan@gmail.com>
2021-03-18 00:57:09 +0700

    add marzipano files

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-among-us",
+      "name": "Panorama Among Us",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 1.1278465845395864,
+        "pitch": 0.14276707284775725,
+        "fov": 1.3517668344523721
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1269858375718336,
+          "pitch": 0.2739328255799993,
+          "rotation": 0.7853981633974483,
+          "target": "1-panorama-among-us-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama-among-us-2",
+      "name": "Panorama Among Us 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
